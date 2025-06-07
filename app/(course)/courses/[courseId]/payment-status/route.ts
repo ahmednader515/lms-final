efcf8434b67ae6d0e1d6288c3831679c74a8ec83
Ahmed Nader <ahmednader5012@gmail.com>
2025-06-07 06:12:45 +0300

    payment fix test 9

---
+++ app/(course)/courses/[courseId]/payment-status/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  return NextResponse.next();
+}
+
+export async function POST(req: Request) {
+  return NextResponse.next();
+} 
